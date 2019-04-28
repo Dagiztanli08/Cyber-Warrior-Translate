90da71f340da08814ed7a68feab543156bcf8fda
Dağıztanlı <40044589+Dagiztanli08@users.noreply.github.com>
2019-04-28 23:04:11 +0300

    Create çeviri.py
    Çeviri dosyası

---
+++ çeviri.py
@@ -0,0 +1,54 @@
+from googletrans import Translator
+import pyperclip
+import sqlite3
+from time import sleep
+from subprocess import check_call
+from os import name as osnames
+
+def baslangic():
+    print("""
+OFFLİNE-ONLİNE
+    TRANSLATOR.
+        CODED FOR CYBER-WARRİOR
+            DAĞIZTANLI08.
+    Eğer cümle kopyalarsanız cevap vermiyecektir.
+
+Twitter = 08Ztanl
+""")
+
+baslangic()
+sleep(0.3)
+
+m = sqlite3.connect("ceviri.db")
+
+def veri_ekle(value1,value2):
+    m_2 = m.cursor()
+    m_2.execute("CREATE TABLE IF NOT EXISTS EMRE('kelime','ceviri')")
+    m_2.execute("INSERT INTO EMRE(kelime,ceviri) SELECT '{}','{}' WHERE NOT EXISTS(SELECT * FROM EMRE WHERE kelime = '{}' AND ceviri = '{}')".format(a,c,a,c))
+    m.commit()
+def veri_cekme(value_1):
+    m_2 = m.cursor()
+    m_2.execute("select * from EMRE where kelime = '{}'".format(value_1))
+    l = m_2.fetchall()
+    return(l)
+
+a = ""
+while True:
+    c = pyperclip.paste()
+    if " " in c and len(c) > 16:
+        continue
+    elif c != a:
+        a = c
+        if len(veri_cekme(a)) != 0:
+            if osnames == "posix":
+                s_1 = check_call("notify-send '{}' '{}'".format("ÇIKTI",veri_cekme(a)), shell = True)
+            print(veri_cekme(a),"Databaseden alındı.")
+        else:
+            try:
+                c = Translator().translate(a,dest='tr').text
+                if osnames == "posix":
+                    s_1 = check_call("notify-send {} {}".format(a,c), shell = True) 
+                print("{} == {}  /////// İnternet kullanıldı".format(a,c))
+                veri_ekle(a,c)
+            except __import__('requests').exceptions.ConnectionError:
+                print("İnternet bağlantısı olmayabilir.")
